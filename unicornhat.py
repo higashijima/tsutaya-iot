4dc68b512cdc4a6c8239155b8ccc43f34ad3b283
higashi <boctok.ctpoba@gmail.com>
2019-12-08 14:07:50 +0000

    add main

--- unicornhat.py
+++ unicornhat.py
@@ -37,19 +37,23 @@ class unicorn:
 
     self.draw_disp(image)
 
+  # テキスト表示
+  def disp_text(self, draw, pos, text, color):
+    draw.text(pos, text, fill=color, font=self.font)
+
   # アイコン表示
-  def disp_icon(iconpath):
+  def disp_icon(self, iconpath):
     image = Image.open(iconpath)
     for ox in range(int(image.size[0]/self.width)):
       for oy in range(int(image.size[1]/self.height)):
         valid = False
         for x in range(self.width):
           for y in range(self.height):
-            pixel = image.getpixel(((ox*width)+y, (oy*height)+x))
-            r, g, b = int(pixel), int(pixel[1]), int(pixel[2])
+            pixel = image.getpixel(((ox*self.width)+y, (oy*self.height)+x))
+            r, g, b = int(pixel[0]), int(pixel[1]), int(pixel[2])
             if r or g or b:
               valid = True
-            hat.set_pixecl(x, y, r, g, b)
+            hat.set_pixel(x, y, r, g, b)
         if valid:
           hat.show()
           time.sleep(self.cycle)
@@ -59,6 +63,13 @@ if __name__=='__main__':
   u = unicorn()
   u.disp_clock((0, 220, 220))
   time.sleep(0.5)
-  u.disp_icon('./icon/us.png') 
-  time.sleep(0.5)
-  u.disp_icon('./icon/weather/cloud.png')
+  u.disp_icon('./icons/us.png') 
+  time.sleep(2)
+  u.disp_icon('./icons/weather/cloudy.png')
+  time.sleep(1)
+  image, draw = u.init_disp()
+  u.disp_text(draw, (0,-1), '1023mb', (255,255,0))
+  u.disp_text(draw, (0,8), '23C', (255,0,240))
+
+  hat.clear()
+  u.draw_disp(image)
