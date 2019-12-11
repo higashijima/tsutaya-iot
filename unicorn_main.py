e0e8ba658dac6291b63071c71d5fe776ea0352ff
isaax <boctok.ctpoba@gmail.com>
2019-12-11 10:04:20 +0000

    add display mode

--- unicorn_main.py
+++ unicorn_main.py
@@ -8,6 +8,7 @@ logger.addHandler(StreamHandler(stream=sys.stdout))
 import paho.mqtt.client as mqtt
 from unicornhat import unicorn
 import unicornhathd as hat
+import datetime
 import time
 
 MQTT_HOST = os.environ.get('MQTT_HOST')
@@ -15,8 +16,10 @@ MQTT_USER = os.environ.get('MQTT_USER')
 MQTT_PASS = os.environ.get('MQTT_PASS')
 MQTT_PORT = int(os.environ.get('MQTT_PORT'))
 MQTT_TOPIC = os.environ.get('MQTT_TOPIC')
+DISP_MODE = os.environ.get('DISP_MODE')
 
 files = {'A': 'america', 'B': 'england', 'C': 'india', 'D': 'brasil'}
+
 
 def main():
     # debug mode when environmet set DEBUG 
@@ -39,7 +42,19 @@ def main():
             payload = msg.payload.decode('utf-8')
             logger.debug('%s retain=%s qos=%s [%s] %s', sendTime, retain, qos, topic, payload)
             rmap = {'time': '12:30', 'weather': '曇り', 'temp': '32'}
-            u.disp_icon('./icons/'+files[payload]+'.png')
+            if DISP_MODE == 'flag':
+                u.disp_icon('./icons/'+files[payload]+'.png')
+            if DISP_MODE == 'temp':
+                now = "{0:%H:%M}".format(datetime.datetime.now())
+                temp = '13C'
+                image, draw = u.init_disp()
+                
+                u.disp_text(draw, (0, -1), now, (255,255,0))
+                u.disp_text(draw, (0, 8), temp, (255,0,255))
+                u.draw_disp(image)
+            if DISP_MODE == 'weather':
+                weather = 'cloudy'
+                u.disp_icon('./icons/weather/'+weather+'.png')
 
         except Exception as e:
             logger.exception(e)
