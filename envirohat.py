302fa97c811bae40996e2005eec7b57bcb2c190c
higashijima <boctok.ctpoba@gmail.com>
2019-12-31 13:09:51 +0000

    enviro commit

--- envirohat.py
+++ envirohat.py
@@ -4,15 +4,18 @@ import time
 from envirophat import light, weather, motion, analog
 
 class enviro:
-  def getJsonDatas():
-    a = weather.altitude()
-    t = weather.templature()
-    p = weather.pressure(unit='hPa')
+  def __init__(self, unit):
+    self.unit = unit
 
-    return '"altitude": {a:.2f}, "templature": {t:.2f}, "pressure": {p:.2f}'.format(a, t, p)
+  def getJsonDatas(self):
+    a = weather.altitude()
+    t = weather.temperature()
+    p = weather.pressure(unit=self.unit)
+
+    return '{{"altitude": {0:.2f}, "templature": {0:.2f}, "pressure": {0:.2f}}}'.format(a, t, p)
 
 if __name__ == '__main__':
-  e = enviro()
-  d = e.getJsonData()
+  e = enviro('hPa')
+  d = e.getJsonDatas()
   print(d)
 
