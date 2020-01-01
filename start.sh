2c0a7011c8fb872b00696edcde18f07a8fc666ef
higashijima <boctok.ctpoba@gmail.com>
2020-01-01 14:10:08 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-export MQTT_HOST=m10.cloudmqtt.com
-export MQTT_USER=juhtilne 
-export MQTT_PASSWORD=GKqNGG_jAh9N 
-export MQTT_PORT=11515
-export EXEC_MAIN=voice
 script=${EXEC_MAIN}
 python3 ${script}_main.py
